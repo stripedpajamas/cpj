16aba73a6da9f5b4ecd905b43469ed5f652df7ff
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-10-31 14:34:54 -0700

    Use cp inputs/outputs for testing

--- src/test/java/cryptopals/Challenge1Test.java
+++ src/test/java/cryptopals/Challenge1Test.java
@@ -14,15 +14,15 @@ public class Challenge1Test {
 
   @Test
   public void test_hexToBase64() {
-    final String input = "68656c6c6f20776f726c64";
-    final String output = "aGVsbG8gd29ybGQ=";
+    final String input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    final String output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
     Assert.assertEquals(output, challenge1.hexToBase64(input));
   }
 
   @Test
   public void test_base64ToHex() {
-    final String input = "aGVsbG8gd29ybGQ=";
-    final String output = "68656c6c6f20776f726c64";
+    final String input = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+    final String output = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";;
     Assert.assertEquals(output, challenge1.base64ToHex(input));
   }
 }
